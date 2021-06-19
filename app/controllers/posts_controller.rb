8cfd1c5154e7adf21ee17960c67ec0320e256a6a
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-06-19 22:27:07 +0900

    view作成(post/new)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
+  def index
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image)
   end
 end
